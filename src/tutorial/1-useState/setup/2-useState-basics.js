ba91435bbb4dba9824c8f8b65408126c5675e0ea
Ashwin Vijendra <ashwinvijendra@gmail.com>
2021-11-29 16:19:56 +0530

    example on useState array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [value, func1] = useState("Change My Title :(");
+
+  const handleClick = () => {
+    value === "Change My Title :("
+      ? func1("Title Changed :)")
+      : func1("Change My Title :(");
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+      <button type="button" onClick={handleClick}>
+        Click Me
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
