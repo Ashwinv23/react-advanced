1ac46e54d98209f1bdbab6e9d07d668dc8f62753
Ashwin Vijendra <ashwinvijendra@gmail.com>
2021-12-02 18:45:53 +0530

    useEffect multiple returns alternative code

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -7,24 +7,42 @@ const MultipleReturns = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState("default user");
 
+  // useEffect using async await
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       let response = await fetch(url);
+  //       if (response.status >= 200 && response.status <= 299) {
+  //         response = await response.json();
+  //         setUser(response.login);
+  //       } else {
+  //         setIsError(true);
+  //       }
+  //       setIsLoading(false);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
+  // useEffect using callbacks
   useEffect(() => {
-    async function fetchData() {
-      try {
-        let response = await fetch(url);
-        if (response.status >= 200 && response.status <= 299) {
-          response = await response.json();
-          setUser(response.login);
-          // setIsLoading(false);
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
         } else {
-          // setIsLoading(false);
+          setIsLoading(false);
           setIsError(true);
+          throw new Error(resp.statusText);
         }
+      })
+      .then((user) => {
+        setUser(user.login);
         setIsLoading(false);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    fetchData();
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   if (isLoading) return <h2>Loading...</h2>;
