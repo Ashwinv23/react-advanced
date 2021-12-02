d21b98d8a8b0d3a37f21d737c5a142699a06cfa5
Ashwin Vijendra <ashwinvijendra@gmail.com>
2021-12-02 18:26:58 +0530

    useEffect multiple returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,35 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await fetch(url);
+        if (response.status >= 200 && response.status <= 299) {
+          response = await response.json();
+          const { login } = response;
+          setUser(login);
+          setIsLoading(false);
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error...</h2>;
+  return <h2>{user}</h2>;
 };
 
 export default MultipleReturns;
