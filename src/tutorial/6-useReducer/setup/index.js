cc3d213b4263a9c27ba2dd2cfe3183cccb4ae2e7
Ashwin Vijendra <ashwinvijendra@gmail.com>
2021-12-09 17:50:37 +0530

    Add items through form using useState

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,42 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [people, setPeople] = useState(data);
+  let [modal, setModal] = useState(false);
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      setModal(true);
+      setPeople([...people, { id: new Date().getTime(), name }]);
+    } else {
+      setModal(true);
+    }
+  };
+
+  return (
+    <>
+      {modal && <Modal />}
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <button type='submit'>Add</button>
+      </form>
+      {people.map((ppl) => {
+        return (
+          <div>
+            <h4>{ppl.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
